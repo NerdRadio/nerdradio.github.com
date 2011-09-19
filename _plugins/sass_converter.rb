4ae72ac34464f984772e2d0c87aaecb00098c5a9
Garth Braithwaite <garthdb@gmail.com>
2011-09-19 15:57:31 -0700

    Still working on scss

--- _plugins/sass_converter.rb
+++ _plugins/sass_converter.rb
@@ -1,28 +1,39 @@
 module Jekyll
-  # Sass plugin to convert .scss to .css
-  # 
-  # Note: This is configured to use the new css like syntax available in sass.
-  require 'sass'
-  class SassConverter < Converter
-    safe true
-    priority :low
+  # Compiled Sass into CSS. You must specify an empty YAML front matter
+  # at the beginning of the file.
+  # sass|sccs -> .css
+  class SassConverter < Converter    
+    def setup
+      return if @setup
+      require 'sass'
+      @setup = true
+    rescue LoadError
+      STDERR.puts 'You are missing a library required for sass. Please run:'
+      STDERR.puts '  $ [sudo] gem install sass'
+      raise FatalException.new("Missing dependency: sass")
+    end
 
-     def matches(ext)
-      ext =~ /scss/i
+    def matches(ext)
+      ext =~ /sass|scss/i
     end
 
     def output_ext(ext)
-      ".css"
+      '.css'
     end
 
     def convert(content)
+      setup
       begin
-        puts "Performing Sass Conversion."
-        engine = Sass::Engine.new(content, :syntax => :scss)
-        engine.render
-      rescue StandardError => e
-        puts "!!! SASS Error: " + e.message
+        Sass::Engine.new(content, :style => :compressed, :syntax => syntax(content)).render
+      rescue => e
+        puts "Sass Exception (#{e.sass_line}): #{e.message}"
       end
+    end
+    
+  private
+    
+    def syntax(content)
+      content.include?(';') ? :scss : :sass
     end
   end
 end
