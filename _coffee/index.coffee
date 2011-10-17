7cf9219ed95f12a90ca532884c4e6e445e2bb665
Garth Braithwaite <garthdb@gmail.com>
2011-10-17 16:34:12 -0700

    still playing with coffee

--- _coffee/index.coffee
+++ _coffee/index.coffee
@@ -2,7 +2,7 @@ PadDigits = (n, totalDigits) ->
 	n = n.toString()
 	pad = ''
 	if totalDigits > n.length
-		for i in [0..(totalDigits-n.length)]
+		for i in [1..(totalDigits-n.length)]
 			pad += '0'
 	pad + n.toString()
 
@@ -12,12 +12,18 @@ TimeString = (time) ->
 	sec = PadDigits(sec, 2)
 	min + ":" + sec
 	
-$(document).ready ->
+CommentCount = ->
+	console.log('test')
 	for comment_count in $('.episode .comment_count a')
 		count = PadDigits(parseInt($(comment_count).text()), 3)
 		count_array = count.split('')
 		output = ''
-		for element in count_array
-			output += '<span class="count_'+element+'">'+element+'</span>'
-		$(this).html(comment_count)
+		for digit in count_array
+			output += "<span class=\"count_#{digit}\">#{digit}</span>"
+		$(comment_count).html(output)
+	
+$(document).ready ->
+	console.log('ready')
+	CommentCount()
+	
 
--- _site/js/index.js
+++ _site/js/index.js
@@ -1,11 +1,11 @@
 (function() {
-  var PadDigits, TimeString;
+  var CommentCount, PadDigits, TimeString;
   PadDigits = function(n, totalDigits) {
     var i, pad, _ref;
     n = n.toString();
     pad = '';
     if (totalDigits > n.length) {
-      for (i = 0, _ref = totalDigits - n.length; 0 <= _ref ? i <= _ref : i >= _ref; 0 <= _ref ? i++ : i--) {
+      for (i = 1, _ref = totalDigits - n.length; 1 <= _ref ? i <= _ref : i >= _ref; 1 <= _ref ? i++ : i--) {
         pad += '0';
       }
     }
@@ -18,8 +18,9 @@
     sec = PadDigits(sec, 2);
     return min + ":" + sec;
   };
-  $(document).ready(function() {
-    var comment_count, count, count_array, element, output, _i, _j, _len, _len2, _ref, _results;
+  CommentCount = function() {
+    var comment_count, count, count_array, digit, output, _i, _j, _len, _len2, _ref, _results;
+    console.log('test');
     _ref = $('.episode .comment_count a');
     _results = [];
     for (_i = 0, _len = _ref.length; _i < _len; _i++) {
@@ -28,11 +29,15 @@
       count_array = count.split('');
       output = '';
       for (_j = 0, _len2 = count_array.length; _j < _len2; _j++) {
-        element = count_array[_j];
-        output += '<span class="count_' + element + '">' + element + '</span>';
+        digit = count_array[_j];
+        output += "<span class=\"count_" + digit + "\">" + digit + "</span>";
       }
-      _results.push($(this).html(comment_count));
+      _results.push($(comment_count).html(output));
     }
     return _results;
+  };
+  $(document).ready(function() {
+    console.log('ready');
+    return CommentCount();
   });
 }).call(this);
